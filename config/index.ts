2f02c0e89ffbadaa91f3ad29f2e05cb206a3d575
nagaraju123 <nagaraju0503@gmail.com>
2019-06-28 13:26:22 +0530

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,8 +2,8 @@ import * as sequelize from 'sequelize';
 
 export default class Tables {
     createConnection() {
-        let db = new sequelize('YTBDEVDB02', 'ytbadmin', 'Yt6@dm1n1234', {
-            host: 'ytbproddbsvr01.database.windows.net',
+        let db = new sequelize('dbname', 'username', 'pass', {
+            host: 'host',
             dialect: 'mssql',
             dialectOptions: {
                     encrypt: true
